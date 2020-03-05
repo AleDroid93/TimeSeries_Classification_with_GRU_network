28ad23e2b746ce3a06ebc1f01cf3e41c56cd655b
Alessandro Censi <alessandromic.censi@gmail.com>
2020-03-05 14:58:00 +0100

    Modek Saving and Loading added.

--- main_gru.py
+++ main_gru.py
@@ -1,12 +1,14 @@
 import numpy as np
-import constants
+import os
+import tensorflow as tf
+from constants import N_BANDS, N_FOLDS, N_TIMESTAMPS, N_CLASSES, EPOCHS
 from model import MyGRU
 from utils import reshapeToTensor
 from sklearn.metrics import accuracy_score, f1_score, cohen_kappa_score
 from train import train
 
 
-for i in range(constants.N_FOLDS):
+for i in range(N_FOLDS):
     current_fold = str(i + 1)
     train_fn = '[YOUR TRAIN DATA FILE PATH HERE]'
     validation_fn = '[YOUR VALIDATION DATA FILE PATH HERE]'
@@ -29,15 +31,23 @@ for i in range(constants.N_FOLDS):
     y_train = np.load(target_train_fn)
     y_validation = np.load(target_validation_fn)
     y_test = np.load(target_test_fn)
-    n_classes = 12
-    model = MyGRU(512, n_classes, dropout_rate=0.5)
+    model = MyGRU(512, N_CLASSES, dropout_rate=0.5)
     print(x_train.shape)
     print("Fold %s metrics:\n" % current_fold)
     # TRAINING
-    train(model, x_train, y_train, x_validation, y_validation, n_epochs=constants.EPOCHS)
+    outputFolder = './MLP_output'
+    if not os.path.exists(outputFolder):
+        os.makedirs(outputFolder)
 
+    """ defining loss function and the optimizer to use in the training phase """
+    loss_object = tf.keras.losses.CategoricalCrossentropy()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
+
+    ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, model=model)
+    manager = tf.train.CheckpointManager(ckpt, outputFolder + '/MLP_ckpts_fold ' + current_fold, max_to_keep=1)
+    train(model, x_train, y_train, x_validation, y_validation, loss_object, optimizer, ckpt, manager, n_epochs=EPOCHS)
     # TESTING
     pred = model.predict(x_test)
     print("Accuracy score on test set: ", accuracy_score(np.argmax(y_test, axis=1), np.argmax(pred, axis=1)))
-    print("F-score on test set: ", f1_score(y_test, pred, average='macro'))
-    print("K-score on test set: ", cohen_kappa_score(y_test, pred))
+    print("F-score on test set: ", f1_score(np.argmax(y_test, axis=1), np.argmax(pred, axis=1), average='macro'))
+    print("K-score on test set: ", cohen_kappa_score(np.argmax(y_test, axis=1), np.argmax(pred, axis=1)))
